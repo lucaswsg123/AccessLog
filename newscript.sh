cf47495e48303af0c02cbe0f80469e98167b032c
lucaswsg123 <lucaswsg1@gmail.com>
2025-11-28 19:26:56 -0600

    feat: create log file if one doesn't exist

--- newscript.sh
+++ newscript.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 access_dt=$(date "+%b %d %Y %r %Z")
 
+FILE=/access_log.txt
+
 echo "Hello, who are you?"
 read NAME
 
@@ -8,6 +10,11 @@ if [[ -z $NAME ]]
 then
     echo "You didn't even enter a name !!"
 else
+    if [[ -z "$FILE" ]]
+    then
+        touch access_log.txt
+    fi
+
     echo -e "\n$NAME!"
 
     if [[ $NAME == 'Lucas' ]]
