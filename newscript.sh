ac52bfd5601e73c5e36d2267ac692e86df92202e
lucaswsg123 <lucaswsg1@gmail.com>
2025-11-28 19:14:35 -0600

    feat: added response for null user

--- newscript.sh
+++ newscript.sh
@@ -4,14 +4,19 @@ access_dt=$(date "+%b %d %Y %r %Z")
 echo "Hello, who are you?"
 read NAME
 
-echo -e '\n'$NAME!
-if [[ $NAME == 'Lucas' ]]
+if [[ -z $NAME ]]
 then
-    echo Welcome back!
-    echo $NAME successfully logged in at $access_dt >> access_log.txt
+    echo "You didn't even enter a name !!"
 else
-    echo 'Get out of here! >_>'
-    echo $NAME attempted to log in at $access_dt >> access_log.txt
-fi
+    echo -e "\n$NAME!"
 
+    if [[ $NAME == 'Lucas' ]]
+    then
+        echo Welcome back!
+        echo $NAME successfully logged in at $access_dt >> access_log.txt
+    else
+        echo 'Get out of here! >_>'
+        echo $NAME attempted to log in at $access_dt >> access_log.txt
+    fi
+fi
 
